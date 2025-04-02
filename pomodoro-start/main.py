ce65e0f03404f18d7c962e9e2c67a13890d7ca26
Tevyn <tevyn.payne@gmail.com>
2025-04-02 17:18:22 -0500

    adding the timer functinality

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -10,16 +11,35 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_timer():
+    pass
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    countdown(5*60)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def countdown(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_min < 10:
+        count_min = f"0{count_min}"
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, countdown,count -1 )
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
+
+
 
 timer_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 36, "bold"), bg=YELLOW)
 timer_label.grid(row=0, column=1)
@@ -29,9 +49,11 @@ canvas = Canvas(width=200 , height=224, bg=YELLOW, highlightthickness=0)
 tomato = PhotoImage(file="tomato.png")
 canvas.create_image(100 ,112, image=tomato)
 canvas.grid(row=1, column=1)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
 
-start_button = Button(text="Start", bg=YELLOW, highlightthickness=0)
+
+
+start_button = Button(text="Start", bg=YELLOW, highlightthickness=0, command=start_timer)
 start_button.grid(row=2, column=0)
 
 reset_button = Button(text="Reset", bg=YELLOW, highlightthickness=0)
